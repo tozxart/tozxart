4af8fb53ef9839fe4e36a12c2473559e8c5d2590
tozxart <69420764+tozxart@users.noreply.github.com>
2022-03-02 23:57:48 +0100

    Update 3

--- Personal/contact.jsx
+++ Personal/contact.jsx
@@ -1,22 +1,16 @@
-<form action="" method="post" role="form" class="php-email-form mt-4">
-<div class="row">
-  <div class="col-md-6 form-group">
-    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-  </div>
-  <div class="col-md-6 form-group mt-3 mt-md-0">
-    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-  </div>
-</div>
-<div class="form-group mt-3">
-  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-</div>
-<div class="form-group mt-3">
-  <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-</div>
-<div class="my-3">
-  <div class="loading">Loading</div>
-  <div class="error-message"></div>
-  <div class="sent-message">Your message has been sent. Thank you!</div>
-</div>
-<div class="text-center"><button type="submit">Send Message</button></div>
-</form>
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_0xp03b2', 'template_s8bh1hh', form.current, 'B_gDFk1BOuWXrF0q-')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
