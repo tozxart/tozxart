dbfd62048696c64ce4af780b19afafcce0c4704e
tozxart <69420764+tozxart@users.noreply.github.com>
2024-03-22 23:12:29 +0100

    Update WebURLs.lua

--- WebURLs.lua
+++ WebURLs.lua
@@ -1,8 +1,8 @@
 local WebURLs = {}
-WebURLs.Reset = "https://intrudershub.pro/"
-WebURLs.usage = "https://intrudershub.pro/"
-WebURLs.Time = "https://intrudershub.pro/"
-WebURLs.Free = "https://intrudershub.pro/"
-WebURLs.Paid = "https://intrudershub.pro/"
-WebURLs.Copy = "https://intrudershub.pro/"
+WebURLs.Reset = "http://paid3.daki.cc:4026/"
+WebURLs.usage = "http://paid3.daki.cc:4026/"
+WebURLs.Time = "http://paid3.daki.cc:4026/"
+WebURLs.Free = "http://paid3.daki.cc:4026/"
+WebURLs.Paid = "http://paid3.daki.cc:4026/"
+WebURLs.Copy = "http://paid3.daki.cc:4026/"
 return WebURLs
