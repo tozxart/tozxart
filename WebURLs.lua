a8898fc76659c46942284b4d8ef0341e942a96c1
tozxart <69420764+tozxart@users.noreply.github.com>
2024-08-02 18:51:31 +0100

    Update WebURLs.lua

--- WebURLs.lua
+++ WebURLs.lua
@@ -1,8 +1,8 @@
 local WebURLs = {}
-WebURLs.Reset = "http://paid3.daki.cc:4026/"
-WebURLs.usage = "http://paid3.daki.cc:4026/"
-WebURLs.Time = "http://paid3.daki.cc:4026/"
-WebURLs.Free = "http://paid3.daki.cc:4026/"
-WebURLs.Paid = "http://paid3.daki.cc:4026/"
-WebURLs.Copy = "http://paid3.daki.cc:4026/"
+WebURLs.Reset = "http://paid1.daki.cc:4008/"
+WebURLs.usage = "http://paid1.daki.cc:4008/"
+WebURLs.Time = "http://paid1.daki.cc:4008/"
+WebURLs.Free = "http://paid1.daki.cc:4008/"
+WebURLs.Paid = "http://paid1.daki.cc:4008/"
+WebURLs.Copy = "http://paid1.daki.cc:4008/"
 return WebURLs
